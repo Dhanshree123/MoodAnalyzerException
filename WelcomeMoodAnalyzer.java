9dc4d5c100a65e841126ce33aae56a7c057ff93a
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-09-28 23:09:44 +0530

    Adding Master

---
+++ WelcomeMoodAnalyzer.java
@@ -0,0 +1,15 @@
+package MoodAnalyzer;
+
+public class WelcomeMoodAnalyzer {
+	
+
+	public static void main(String[] args) {
+		
+		String msg = "I am sad";
+			if(msg.contains("sad"))
+				System.out.println("SAD");
+			else
+				System.out.println("HAPPY");
+		
+	}
+}
